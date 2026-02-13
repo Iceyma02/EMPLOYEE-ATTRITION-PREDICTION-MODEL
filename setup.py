ff216f67b3d064cd459044332bbf7890fb20b001
Anesu Manjengwa <manjengwap10@gmail.com>
2026-02-13 23:58:28 +0200

    Remove author details from setup.py
    Removed author information from setup.py.

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name="employee-attrition-prediction",
     version="1.0.0",
-    description="Employee Attrition Prediction System for CBZ Holdings",
-    author="Your Name",
-    author_email="your.email@example.com",
     packages=find_packages(),
     install_requires=[
         "pandas>=2.0.0",
